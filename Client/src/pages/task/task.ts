db828b8da2a6ccf53da9138b7b18f6e3de0ffa15
pabon <s.pabon93@gmail.com>
2019-10-10 00:01:04 +0600

    Modal closed event added on task page

--- Client/src/pages/task/task.ts
+++ Client/src/pages/task/task.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
 
 /**
  * Generated class for the TaskPage page.
@@ -15,11 +15,17 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class TaskPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TaskPage');
+  }
+
+  closeTask(){
+
+    this.viewCtrl.dismiss();
+    
   }
 
 
