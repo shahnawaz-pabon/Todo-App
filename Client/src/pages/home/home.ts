b544dd2e84966313230302cc11d060488c5af245
pabon <s.pabon93@gmail.com>
2019-10-28 22:32:26 +0600

    Navbar theme changed, ion segment added into home page

--- Client/src/pages/home/home.ts
+++ Client/src/pages/home/home.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NavController, ModalController, NavParams } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, ModalController, NavParams, Slides } from 'ionic-angular';
 
 // import { TaskPage } from '../task/task';
 
@@ -9,10 +9,24 @@ import { NavController, ModalController, NavParams } from 'ionic-angular';
 })
 export class HomePage {
 
+  @ViewChild('slider') slider: Slides;
+  @ViewChild("segments") segments;
+  page: any;
+
   todoItems: any = [];
+
+  public category: string = 'tasks';
+  public categories: Array<string> = ['tasks', 'stats', 'trash']
 
   constructor(public navCtrl: NavController, public modal: ModalController, public navParams: NavParams) {
 
+    // this.slides.freeMode = true;
+
+  }
+
+  // Initialize slider
+  ionViewDidEnter(){
+    this.slideChanged();
   }
 
   openTaskModal(){
@@ -32,6 +46,25 @@ export class HomePage {
 
   todoToggle(todoItem){
     console.log(todoItem);
+  }
+
+  // On segment click
+  selectedTab(index) {
+    this.slider.slideTo(index);
+    console.log("selectedTab",index)
+  }
+
+  // On slide changed
+  slideChanged() {
+    let currentIndex = this.slider.getActiveIndex();
+    let slides_count = this.segments.nativeElement.childElementCount;
+
+    this.page = currentIndex.toString();
+    if(this.page >= slides_count)
+      this.page = (slides_count-1).toString();
+
+    console.log("slides_count",slides_count)
+    console.log("this.page",this.page)
   }
 
 }
